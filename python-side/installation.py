db5cb57dd69086de7c364b483fe8bb54c49155e7
Relative <kezin636@gmail.com>
2025-03-06 17:58:42 +0300

    added apt pacman wrapper

--- python-side/installation.py
+++ python-side/installation.py
@@ -117,7 +117,7 @@ def start_installation(installation_object: InstallInfo):
                 "pamac", "apple-fonts", "yay",
                 "p7zip", 
                 "zip", "unzip", "unrar", "neofetch",
-                "sof-firmware", "fastfetch", "btop"
+                "sof-firmware", "fastfetch", "btop", "aptpac"
             ] + de_packages
         ):
         failmsg()
