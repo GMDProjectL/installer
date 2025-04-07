60b890620598293db45c8373acb6c00c99c261d1
ShineUA <playimit@gmail.com>
2025-04-07 17:38:32 +0200

    fix of the previous fix

--- new-imgui/src/components/titletext.cpp
+++ new-imgui/src/components/titletext.cpp
@@ -5,10 +5,7 @@
 
 void Components::TitleText(const char *text) {
     ImGui::PushFont(StyleShit::g_titleFont);
-
-    ImGui::SetCursorPosX((ImGui::GetWindowWidth() - ImGui::CalcTextSize(text).x) / 2 + 10);
     ImGui::SetCursorPosX((ImGui::GetWindowWidth() - ImGui::CalcTextSize(text).x) / 2);
-
     ImGui::Text("%s", text);
     ImGui::PopFont();
 }
