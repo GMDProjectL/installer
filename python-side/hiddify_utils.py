ba25a91336e881ab9c2024193dae6a567f02d0f8
sh1ne_dev <playimit@gmail.com>
2025-07-05 08:23:36 +0200

    change hiddify repo to GMDProjectL/hiddify

--- python-side/hiddify_utils.py
+++ python-side/hiddify_utils.py
@@ -5,7 +5,7 @@ from pacman_utils import pacman_install_from_file
 
 
 def get_latest_hiddify_release():
-    url = 'https://api.github.com/repos/GMDProjectL/hiddify-next-bin/releases'
+    url = 'https://api.github.com/repos/GMDProjectL/hiddify/releases'
     response = requests.get(url)
     result = response.json()
 
@@ -17,6 +17,7 @@ def get_zstd():
 
 def download_zstd(url: str, dest: str):
     response = requests.get(url, stream=True)
+    print(dest)
     with open(dest, 'wb') as f:
         for chunk in response.iter_content(chunk_size=1024): 
             if chunk:
@@ -25,7 +26,7 @@ def download_zstd(url: str, dest: str):
 def install_hiddify(installation_object: InstallInfo, root: str):
     try:
         zstd_url = get_zstd()
-        zstd_dest = '/var/cache/pacman/pkg/hiddify-next-bin.pkg.tar.zstd'
+        zstd_dest = '/var/cache/pacman/pkg/hiddify.pkg.tar.zstd'
         download_zstd(zstd_url, root + zstd_dest)
         pacman_install_from_file(installation_object, root, zstd_dest)
         
