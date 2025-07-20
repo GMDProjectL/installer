c5e5e966d76b34cf408efa9433773580f522ffad
Andrew <kezin636@gmail.com>
2025-07-20 18:21:18 +0300

    removed legacy geode installer

--- python-side/apps/geode.py
+++ python-side/apps/geode.py
@@ -1,8 +1,15 @@
+import os
 from shared import shared_events
 from apps.github import install_latest_gh_package
+from base.pacman import pacman_remove
 
 
 def install_geode_installer(root: str):
     shared_events.append('Installing Geode Installer...')
+
+    if os.path.exists('/geode-installer/main.py'):
+        shared_events.append('Removing legacy geode-installer...')
+        pacman_remove(root, 'geode-installer')
+        return
     
-    install_latest_gh_package(root, 'GMDProjectL/geode-installer', 'geode-installer')
+    install_latest_gh_package(root, 'GMDProjectL/geode-linux-installer', 'geode-linux-installer')
