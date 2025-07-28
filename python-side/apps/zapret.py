4282894775f097cd7f02fed095a3d9f2898390fb
Andrew <kezin636@gmail.com>
2025-07-28 19:11:14 +0300

    it might work

--- python-side/apps/zapret.py
+++ python-side/apps/zapret.py
@@ -6,10 +6,5 @@ from base.process import run_command_in_chroot
 
 def install_zapret(root: str):
     shared_events.append('Installing Zapret...')
-
-    if os.path.exists('/opt/zapret'):
-        shared_events.append('Removing legacy zapret...')
-        run_command_in_chroot(root, ['rm', '-rf', '/opt/zapret'])
-        return
     
     install_latest_gh_package(root, 'GMDProjectL/zapret-installer', 'zapret-installer')
